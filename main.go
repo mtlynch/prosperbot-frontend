7db7a7201503f11103e341012859c2dd4cc7f8c3
Michael Lynch <mtlynch@users.noreply.github.com>
2016-11-06 11:46:15 -0500

    Deleting ServeSingle because it's dead code now (#8)

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 	"github.com/mtlynch/prosperbot-frontend/notes"
 )
 
-func serveSingle(pattern string, filename string) {
-	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Requested static file: %v\n", r.URL.Path)
-		http.ServeFile(w, r, filename)
-	})
-}
-
 func main() {
 	p := flag.Int("port", 8082, "port on which to listen for web requests")
 	flag.Parse()
